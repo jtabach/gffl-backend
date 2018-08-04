8f911015b4c22dedda58bbe1a7616667dc4e402a
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-04 12:03:02 +0200

    store users in mongo and set session cookie

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
@@ -15,6 +18,15 @@ app.use(function(req, res, next) {
   );
   next();
 });
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes')(app);
 require('./routes/authRoutes')(app);
--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,39 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, profile);
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // we already have a record with the profile id
+        done(null, existingUser);
+      } else {
+        // we don't have a record with this id, make a new record
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, existingUser);
+      }
     }
   )
 );
