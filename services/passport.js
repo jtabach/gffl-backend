318409f447a6ad1dc6920fe4696943a048cbf3a1
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-04 13:42:37 +0200

    absolute path for google auth callback

--- services/passport.js
+++ services/passport.js
@@ -20,8 +20,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy: true
+      callbackURL: 'https://gffl-backend.herokuapp.com/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id });
