695d82d330c1c72906034c988303eff59ee9d1d1
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-04 11:14:01 +0200

    add mongoose config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
-const mongoose = require('mongoose');
-
-// mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
