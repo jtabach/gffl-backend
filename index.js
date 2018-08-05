d99234c6d4b20cd2f5fe98cc1899a84f0b68f67c
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-05 12:21:57 +0200

    move cookieSession and cors into service

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
@@ -10,27 +9,9 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
-  );
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-});
+require('./services/cors')(app);
+require('./services/cookieSession')(app);
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
 app.use(passport.initialize());
 app.use(passport.session());
 
