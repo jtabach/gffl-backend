c55ca0d17bf2b96e904c4fa2f122d786a2b742c1
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-07 17:46:02 +0200

    layout instructions for creating a new league

--- models/League.js
+++ models/League.js
@@ -11,6 +11,22 @@ leagueSchema = new Schema({
   teams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team' }]
 });
 
+leagueSchema.statics.createLeague = (req, res, next) => {
+  // get authToken from req.cookies
+  // decode cookie to get the user._id
+  // save new league to DB
+  // in next():
+  // save new team to DB --> Team.createTeam
+  // save new team to League model array of teams --> League.addTeamToLeague
+  // save new league to User model array of leagues
+  // save new team to User model array of teams
+};
+
+leagueSchema.statics.addTeamToLeague = (req, res, next) => {
+  // takes leagueId and UserId from res
+  // fetch league by league Id and push team to teams array
+};
+
 const League = mongoose.model('leagues', leagueSchema);
 
 module.exports = League;
---
+++ routes/api/league.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const League = require('../../models/League');
+
+router.post('/', League.createLeague, (req, res) => {
+  res.send('league');
+});
