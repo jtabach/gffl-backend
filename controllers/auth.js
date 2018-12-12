1ecd15516738ac55c37c71a21b64c0fd6898e554
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-12 14:13:33 +0900

    convert register to async/await

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,46 +12,33 @@ const AuthController = {
   logout
 };
 
-function register(req, res, next) {
+async function register(req, res, next) {
   const email = req.body.email.toLowerCase();
   const password = req.body.password;
 
   // check if user or email already exists
-  mongoose.model('User').findOne({ email: email }, (err, foundUser) => {
-    if (err) {
-      return res.status(400).send({
-        verify: false,
-        message: 'Something went wrong. Please try again'
-      });
-    }
-    if (foundUser) {
-      return res
-        .status(400)
-        .send({ verify: false, message: 'Email has already been taken' });
-      // return next('Email has already been taken');
-    }
+  const foundUser = await mongoose.model('User').findOne({ email: email });
+  if (foundUser) {
+    return res
+      .status(400)
+      .send({ verify: false, message: 'Email has already been taken' });
+  }
 
-    // create new user instance
-    let user = new User();
-    // hash plaintext password
-    bcrypt.hash(password, 10, function(err, hash) {
-      // Store hash in db
-      user.email = email;
-      user.password = hash;
-      user.save((err, savedUser) => {
-        if (err) {
-          return res.status(400).send({
-            verify: false,
-            message: 'Something went wrong. Please try again'
-          });
-        }
-        const authToken = helper.encodeAuthToken(savedUser);
-        res.cookie('authToken', authToken);
-        res.user = savedUser;
-        return res.status(200).send({ user: res.user });
-      });
-    });
-  });
+  let user = new User();
+
+  const hash = await bcrypt.hash(password, 10);
+
+  user.email = email;
+  user.password = hash;
+
+  try {
+    const savedUser = await user.save();
+    const authToken = helper.encodeAuthToken(savedUser);
+    res.cookie('authToken', authToken);
+    return res.status(200).send({ user: savedUser });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 }
 
 function login(req, res, next) {
