f51f03cf429a403cfdbe51a55da96623eb7914a8
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-12 22:02:03 +0900

    convert createNotification to async/await

--- controllers/notification.js
+++ controllers/notification.js
@@ -34,54 +34,40 @@ async function getNotifications(req, res, next) {
   }
 }
 
-function createNotification(req, res, next) {
+async function createNotification(req, res, next) {
   let { authToken } = req.cookies;
   let user = helper.decodeAuthToken(authToken);
   const { leagueId, verb, actingOn } = req.body;
   const actor = user._id;
 
-  // find all teams by league Id + populate Users in Team
-  mongoose.model('League').findById(leagueId, (err, foundLeague) => {
-    if (err) return res.status(400).send(err);
+  try {
+    const foundLeague = await mongoose.model('League').findById(leagueId);
+    const populatedLeague = await mongoose.model('League').findById(leagueId).populate({ path: 'teams'});
 
-    foundLeague.populate('teams', (err, populatedLeague) => {
-      if (err) return res.status(400).send(err);
+    populatedLeague.teams.forEach(async team => {
+      const foundUser = await mongoose.model('User').findById(team.user);
 
-      var completed = []; // TODO: fix temporary resolver for looping through async actions
+      // TODO: check if user preferences for recieving notification
+      // TODO: check if user is also the actor
+      const newNotification = new Notification();
 
-      populatedLeague.teams.forEach(team => {
-        mongoose.model('User').findById(team.user, (err, foundUser) => {
-          if (err) return res.status(400).send(err);
+      newNotification.user = foundUser._id;
+      newNotification.verb = verb;
+      newNotification.actor = actor;
+      newNotification.league = leagueId;
+      newNotification.actingOn = actingOn;
+      newNotification.hasViewed = false;
+      newNotification.hasDismissed = false;
 
-          // TODO: check if user preferences for recieving notification
-          // TODO: check if user is also the actor
-          const newNotification = new Notification();
-
-          newNotification.user = foundUser._id;
-          newNotification.verb = verb;
-          newNotification.actor = actor;
-          newNotification.league = leagueId;
-          newNotification.actingOn = actingOn;
-          newNotification.hasViewed = false;
-          newNotification.hasDismissed = false;
-
-          foundUser.notifications.push(newNotification);
-          foundUser.save(err => {
-            if (err) return res.status(400).send(err);
-
-            newNotification.save(err => {
-              if (err) return res.status(400).send(err);
-
-              completed.push('done'); // TODO: fix temporary resolver for looping through async actions
-              if (completed.length == populatedLeague.teams.length) {
-                return res.status(200).send({ success: true });
-              }
-            });
-          });
-        });
-      });
+      foundUser.notifications.push(newNotification);
+      await foundUser.save();
+      await newNotification.save();
     });
-  });
+
+    return res.status(200).send({ success: true });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 }
 
 async function viewNotification(req, res, next) {
