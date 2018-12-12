2d50e77bf2e96c54dd7c9ad6060b6371d2807b66
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-12 13:39:39 +0900

    fix logic for found teams

--- controllers/team.js
+++ controllers/team.js
@@ -44,35 +44,32 @@ async function createTeam(req, res, next) {
   } catch {
     return res.status(400).send({ message: 'LeagueId does not exist' });
   }
-  console.log('step 1');
-  try {
-    await Team.findOne({ user: newTeam.user, league: newTeam.league });
-  } catch {
+
+  const errorTeam = await Team.findOne({ user: newTeam.user, league: newTeam.league });
+  if (errorTeam) {
     return res.status(400).send({ message: 'You already have a team in this league' })
   }
-  console.log('step 2');
 
-  try {
-    await mongoose.model('Team').findOne({ name: newTeam.name, league: newTeam.league });
-  } catch {
+  const duplicateTeam = await mongoose.model('Team').findOne({ name: newTeam.name, league: newTeam.league });
+  if (duplicateTeam) {
     return res.status(400).send({ message: 'This team name has already been taken in this league' })
   }
-  console.log('step 3');
 
   try {
     await newTeam.save();
+
     foundLeague.teams.push(newTeam._id);
     await foundLeague.save();
+
     const foundUser = await mongoose.model('User').findById(newTeam.user);
     foundUser.teams.push(newTeam._id);
     await foundUser.save();
-    const populatedTeam = await helper.populateTeam(newTeam);
-    res.status(200).send({})
-  } catch (err) {
-    return res.status(400).send({ team: populatedTeam });
-  }
 
-    // .then(() => helper.populateTeam(newTeam, res, next));
+    const populatedTeam = await helper.populateTeam(newTeam);
+    res.status(400).send({ team: populatedTeam })
+  } catch (err) {
+    return res.status(400).send(err)
+  }
 }
 
 module.exports = TeamController;
