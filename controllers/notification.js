5847876aee8aa1d6cfd8e7cae9a60f80524af6ac
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-12 21:30:53 +0900

    convert viewAllNotifications to async/await

--- controllers/notification.js
+++ controllers/notification.js
@@ -99,26 +99,21 @@ async function viewNotification(req, res, next) {
 }
 
 // TODO: DRY shares logic with dismissNotifications
-function viewAllNotifications(req, res, next) {
+async function viewAllNotifications(req, res, next) {
   const notifications = req.body;
-  var savedNotifications = []; // TODO: fix temporary resolver for looping through async actions
 
-  notifications.forEach(notification => {
-    mongoose
-      .model('Notification')
-      .findById(notification._id, (err, foundNotification) => {
-        if (err) return res.status(400).send(err);
+  try {
+    const savedNotifications = notifications.map(async notification => {
+      const foundNotification = await mongoose.model('Notification').findById(notification._id);
+      foundNotification.hasViewed = true;
+      const savedNotification = await foundNotification.save();
+      return savedNotification;
+    })
 
-        foundNotification.hasViewed = true;
-        foundNotification.save((err, savedNotification) => {
-          savedNotifications.push(savedNotification); // TODO: fix temporary resolver for looping through async actions
-
-          if (savedNotifications.length == notifications.length) {
-            return res.status(200).send({ notifications: savedNotifications });
-          }
-        });
-      });
-  });
+    return res.status(200).send({ notifications: savedNotifications });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 }
 
 function dismissNotifications(req, res, next) {
